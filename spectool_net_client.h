0a89ff4a8fcae5230de0b4cab6725165ed2e1b44
hydratim <timothy.armstrong@duxdigital.co.uk>
2020-08-22 22:25:11 +0100

    Added missing definition

--- spectool_net_client.h
+++ spectool_net_client.h
@@ -134,6 +134,7 @@ void spectool_netcli_setbufferwrite(spectool_server *sr, int buf);
 int spectool_netcli_getwritepend(spectool_server *sr);
 int spectool_netcli_getwritefd(spectool_server *sr);
 int spectool_netcli_poll(spectool_server *sr, char *errstr);
+int spectool_netcli_writepoll(spectool_server *sr, char *errstr);
 spectool_phy *spectool_netcli_enabledev(spectool_server *sr, unsigned int dev_id,
 									 char *errstr);
 int spectool_netcli_disabledev(spectool_server *sr, spectool_phy *dev);
